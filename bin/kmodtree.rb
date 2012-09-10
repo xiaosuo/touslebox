cc10698c1ce62e475e6b8dc9f7f8ec35ddcf746d
Changli Gao <xiaosuo@gmail.com>
2012-09-10 23:37:31 +0800

    Kmodtree: add more options

--- bin/kmodtree.rb
+++ bin/kmodtree.rb
@@ -14,20 +14,30 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
 
-if ARGV.length != 1 and ARGV.length != 2
-  puts "Usage: #{$0} filename [module]"
-  exit 1
-end
-fn = ARGV[0]
-if ARGV.length == 2
-  target = ARGV[1]
-else
-  target = nil
-end
+require 'optparse'
+
+options = {:fn => "/proc/modules"}
+
+OptionParser.new do |opts|
+  opts.banner = "Usage: #{$0} [options] [MODULE]..."
+
+  opts.separator("")
+  opts.separator("Options:")
+
+  opts.on("-f", "--filename FILENAME", "the path of modules") do |f|
+    options[:fn] = f
+  end
+
+  opts.on("-h", "--help", "Show this message") do |h|
+    puts opts
+    exit
+  end
+end.parse!(ARGV)
 
 child = Hash.new
-IO.foreach(fn) do |l|
+IO.foreach(options[:fn]) do |l|
   mod, o1, o2, deps = l.split
   if deps == '-'
     deps = nil
@@ -35,6 +45,11 @@ IO.foreach(fn) do |l|
     deps = deps.split(',')
   end
   child[mod] = deps
+end
+if ARGV.length > 0
+  mods = ARGV
+else
+  mods = child.keys
 end
 
 parent = Hash.new
@@ -68,23 +83,17 @@ def print_ascent(parent, m, prefix)
   end
 end
 
-if target
-  if not child.include?(target)
-    puts "#{target} isn't loaded"
+first = true
+mods.each do |m|
+  if not child.include?(m)
+    puts "#{m} isn't loaded"
     exit 1
   end
-  
-  puts target
-  print_ascent(parent, target, "")
-else
-  first = true
-  child.each_key do |m|
-    if first
-      first = false
-    else
-      puts
-    end
-    puts m
-    print_ascent(parent, m, "")
+  if first
+    first = false
+  else
+    puts
   end
+  puts m
+  print_ascent(parent, m, "")
 end
