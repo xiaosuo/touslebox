b0951f99af3c167632399b4d5ebda196b3975d32
Changli Gao <xiaosuo@gmail.com>
2017-04-04 12:53:06 +0800

    malloc_stats: Use option -batch instead

--- bin/malloc_stats.sh
+++ bin/malloc_stats.sh
@@ -11,8 +11,7 @@ PID=$1
 if grep -q jemalloc /proc/$PID/maps; then
 	gdb attch $PID \
 		-ex 'call malloc_stats_print((void*)0, (void*)0, (void*)0)' \
-		-ex 'set confirm off' \
-		-ex 'quit'
+		-batch
 else
-	gdb attch $1 -ex 'call malloc_stats()' -ex 'set confirm off' -ex 'quit'
+	gdb attch $1 -ex 'call malloc_stats()' -batch
 fi
