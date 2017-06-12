700294ac57533655d596d34de5127aa618d6d414
Changli Gao <xiaosuo@gmail.com>
2017-06-12 13:09:28 +0800

    malloc_stats.sh: Make it work with libraries in other directories

--- bin/malloc_stats.sh
+++ bin/malloc_stats.sh
@@ -8,10 +8,18 @@ if [ $# -ne 1 ]; then
 fi
 PID=$1
 
+SOLIB_PATH=`cat /proc/$PID/maps | awk '/\// {gsub("/[^/]*$", "", $NF); s[$NF]=1} END{ORS=":"; for(k in s){print k}}'`
+SOLIB_PATH=${SOLIB_PATH%:}
 if grep -q jemalloc /proc/$PID/maps; then
-	gdb attach $PID \
+	gdb \
+		-ex "set solib-search-path $SOLIB_PATH" \
+		-ex "attach $PID" \
 		-ex 'call malloc_stats_print((void*)0, (void*)0, (void*)0)' \
 		-batch
 else
-	gdb attach $1 -ex 'call malloc_stats()' -batch
+	gdb \
+		-ex "set solib-search-path $SOLIB_PATH" \
+		-ex "attach $PID" \
+		-ex 'call malloc_stats()' \
+		-batch
 fi
