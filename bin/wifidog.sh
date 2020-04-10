5295042f26fc3c2e877f633c4d6814ba136b72c8
Changli Gao <xiaosuo@gmail.com>
2020-04-10 16:41:53 +0800

    wifidog: Retry 3 times before restarting AP

--- bin/wifidog.sh
+++ bin/wifidog.sh
@@ -6,19 +6,27 @@ if [[ $# -ne 1 ]]; then
 fi
 IP=$1
 
+broken_count=0
 while :; do
 	echo -n "Checking airport..."
 	if ping -c 1 -t 1 $IP &>/dev/null; then
+		broken_count=0
 		echo "OK"
-		sleep  3
 	else
+		broken_count=$((broken_count+1))
+		echo "Broken($broken_count)"
+	fi
+	if [[ $broken_count -eq 0 ]]; then
+		sleep 3
+	elif [[ $broken_count -lt 3 ]]; then
 		sleep 1
-		echo "Broken"
+	else
 		echo -n "Restarting airport..."
 		networksetup -setairportpower airport off &>/dev/null
 		sleep 3
 		networksetup -setairportpower airport on &>/dev/null
 		echo "Done"
 		sleep 10
+		broken_count=0
 	fi
 done
